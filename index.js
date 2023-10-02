70cde3fdf5dbf52da3fd765e37180f3080817831
unknown <vashivan116@gmail.com>
2023-10-02 22:33:00 +0800

    connected database

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+const connectDb = require ('./src/config/database');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+
+// Connect to the database
+connectDb();
 
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json());
