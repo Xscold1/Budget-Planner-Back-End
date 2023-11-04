d1d7b8b44546008c432f21823fe9776a04bb6b5d
DESKTOP-JEK4R85\ynahr <vashivan116@gmail.com>
2023-11-04 22:26:08 +0800

    rcomment throw on finduserid

--- src/utils/findUserId.js
+++ src/utils/findUserId.js
@@ -4,6 +4,6 @@ const ERROR_MESSAGE = require('../constants/error-message')
 module.exports =  findUserId = async (email) => {
   const getUserId = await USER.findOne({email:email});
 
-  if(!getUserId) throw (ERROR_MESSAGE.USER_ERROR_DO_NOT_EXIST)
+  // if(!getUserId) throw (ERROR_MESSAGE.USER_ERROR_DO_NOT_EXIST)
   return getUserId._id
 }
